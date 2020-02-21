74a2f3171dd92903de9f8d276c35285b62888f15
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2020-02-21 20:29:29 -0300

    Aula 1

---
+++ Aula 1/Leilao/Leilao/Avaliador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leilao
+{
+    class Avaliador
+    {
+        private double maiorDeTodos = Double.MinValue;
+        private double menorDeTodos = Double.MaxValue;
+        public double MaiorLance { get; private set; }
+        public double MenorLance { get; private set; }
+        public void Avalia(Leilao leilao)
+        {
+            foreach(var lance in leilao.Lances)
+            {
+                if(lance.Valor > maiorDeTodos)
+                {
+                    maiorDeTodos = lance.Valor;
+                }else if (lance.Valor < menorDeTodos)
+                {
+                    menorDeTodos = lance.Valor;
+                }
+                this.MaiorLance = maiorDeTodos;
+                this.MenorLance = menorDeTodos;
+            }
+        }
+    }
+}
---
+++ Aula 1/Leilao/Leilao/TesteDoAvaliador.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leilao
+{
+    class TesteDoAvaliador
+    {
+        static void Main(string[] args)
+        {
+            //Criar os usuarios
+            Usuario Sonic = new Usuario("Jonatas");
+            Usuario Knuckles = new Usuario("Matheus");
+            Usuario Eggman = new Usuario("Andrew");
+            Usuario Amy = new Usuario("Malu");
+
+            Leilao leilao = new Leilao("Esmeralda do Caos");
+            leilao.Propoe(new Lance(Knuckles, 500));
+            leilao.Propoe(new Lance(Eggman, 650));
+            leilao.Propoe(new Lance(Amy, 1500));
+            leilao.Propoe(new Lance(Sonic, 700));
+            Avaliador leiloeiro = new Avaliador();
+            leiloeiro.Avalia(leilao);
+
+            Console.WriteLine(leiloeiro.MaiorLance);
+            Console.WriteLine(leiloeiro.MenorLance);
+
+            Console.ReadKey();
+        }
+    }
+}
